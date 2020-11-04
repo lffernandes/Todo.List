f946fd9a76d003d9ce54df9bc80228b9b5b4e437
unknown <luiz.fernandesgti@gmail.com>
2020-11-04 16:27:18 -0300

    Create: Domain Entities TodoList

---
+++ Todo.Domain/Entities/TodoList.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace Todo.Domain.Entities
+{
+    public class TodoList : Entity
+    {
+        public TodoList(string title, string user, DateTime date)
+        {
+            Title = title;
+            Status = true;
+            Date = date;
+            User = user;
+        }
+
+        public string Title { get; private set; }
+
+        public bool Status { get; private set; }
+
+        public DateTime Date { get; private set; }
+
+        public string User { get; private set; }
+
+        public void MarkAsEnable()
+        {
+            Status = true;
+        }
+
+        public void MarkAsDisable()
+        {
+            Status = false;
+        }
+
+        public void UpdateTitle(string title)
+        {
+            Title = title;
+        }
+
+    }
+
+}
